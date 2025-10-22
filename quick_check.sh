578307f55fd3d324bdd979f70b386ba5422d2724
Jeff Goeders <jeff.goeders@gmail.com>
2025-10-21 23:07:07 -0600

    Add quick_check.sh

--- quick_check.sh
+++ quick_check.sh
@@ -1,1 +1,22 @@
-python src/infer.py models/sage.pt datasets/dataset1/design_4000/design_4000.dump
+#!/bin/bash
+
+# Checking accuracy of GCN, SAGE model on dataset 1, designs 4000 to 4024
+# While the paper reports GATv2 results on dataset1, the model provided in 
+# this repository is trained on dataset2 only. Hence, we only report GCN and SAGE.
+echo "design,gcn_accuracy,sage_accuracy,gatv2_accuracy"
+for i in {4000..4024}; do
+    echo -n "dataset1/design_$i,"
+
+    out=$(python src/infer.py models/gcn.pt "datasets/dataset1/design_$i/design_$i.dump" --device cpu)
+    echo -n "$(echo "$out" | awk '{print $2}'),"
+
+    out=$(python src/infer.py models/sage.pt "datasets/dataset1/design_$i/design_$i.dump" --device cpu)
+    echo "$(echo "$out" | awk '{print $2}'),"
+done
+
+# Checking accuracy of GATv2 model on dataset 2, designs 4000 to 4024
+for i in {4000..4024}; do
+    echo -n "dataset2/design_$i,"
+    out=$(python src/infer.py models/gat2_pairnorm.pt "datasets/dataset2/design_$i/design_$i.dump" --device cpu)
+    echo ",,$(echo "$out" | awk '{print $2}'),"
+done
